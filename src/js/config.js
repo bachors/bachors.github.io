63a79abd09cebcb6e9e2dee2b6c99718ed35730a
Ican Bachors <bachor.can@gmail.com>
2025-06-02 00:43:42 +0700

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -11,10 +11,7 @@ tailwind.config = {
                     light: '#000000',
                     dark: '#000000'
                 }
-            },
-            fontFamily: {
-                sans: ['HCo Operator Mono', 'JetBrains Mono', 'Berkeley Mono', 'MonoLisa', 'JetBrains Mono BG', 'Fantasque Sans Mono', 'Geist Mono', 'monospace'],
-            },
+            }
         }
     }
 }
