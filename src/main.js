641d5810dd6a790de496d5ee4ee32d865199415e
bachors <bachor.can@gmail.com>
2026-07-21 00:30:47 +0700

    p

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ function updateThemeIcon() {
 	document.getElementById("theme-icon").className = document.documentElement.classList.contains("dark") ? "fa fa-sun" : "fa fa-moon"
 }
 
-function toggleTheme() {
-	let t = document.documentElement.classList.toggle("dark");
-	localStorage.theme = t ? "dark" : "light", updateThemeIcon()
+function updateBg() {
+	const dark = document.documentElement.classList.contains("dark");
+	document.getElementById("bg").classList.toggle("bg-gelap", dark);
+	document.getElementById("bg").classList.toggle("bg-terang", !dark);
 }
 
-updateThemeIcon();
+function toggleTheme() {
+	let t = document.documentElement.classList.toggle("dark");
+	localStorage.theme = t ? "dark" : "light", updateThemeIcon(), updateBg()
+}
+
+updateThemeIcon(), updateBg();
